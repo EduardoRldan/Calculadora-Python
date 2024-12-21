61148bbb7509895c9f1e9b7fb659cdb7f9a802da
EduardoRldan <163453549+EduardoRldan@users.noreply.github.com>
2024-12-20 23:00:23 -0300

    Lista_Tareas

---
+++ Lista_Tareas/todo-frontend/src/components/TaskList.js
@@ -0,0 +1,30 @@
+// src/components/TaskList.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/tasks/')
+      .then(response => {
+        setTasks(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the tasks!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>{task.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
